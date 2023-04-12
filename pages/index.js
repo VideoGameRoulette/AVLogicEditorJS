1c6de7b4f2fbe2bb7d8fe0227b48111acc823413
Christopher D Couture <c.couture@exsi.ca>
2023-04-11 21:37:04 -0400

    moved Powers to component

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,7 @@
-import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Header from 'components/Header';
-import { MainContainer, SecondaryContainer } from 'components/Containers';
-import { PlusIcon, MinusIcon } from "@heroicons/react/24/solid";
-import { classNames } from 'utils';
+import { MainContainer } from 'components/Containers';
 import HeaderButton from 'components/HeaderButton';
-
-// Simulated Power Flags
-const Powers = {
-  None: 0,
-  Gun: 1 << 0,
-  Nova: 1 << 1,
-  Drill: 1 << 2,
-  Kilver: 1 << 3,
-  AddressDisruptor1: 1 << 4,
-  HighJump: 1 << 5,
-  Labcoat: 1 << 6,
-  Drone: 1 << 7,
-  AddressDisruptor2: 1 << 8,
-  Grapple: 1 << 9,
-  Trenchcoat: 1 << 10,
-  AddressBombs: 1 << 11,
-  DroneTeleport: 1 << 12,
-  ExtendedDroneLaunch: 1 << 13,
-  SudranKey: 1 << 14,
-  RedCoat: 1 << 15,
-  Password: 1 << 16,
-  LongKilver: 1 << 17,
-  FatBeam: 1 << 18,
-  TeleReset: 1 << 19,
-
-  // Helper functions
-  hasPower(powers, power) {
-    return (powers & power) === power;
-  },
-
-  addPower(powers, power) {
-    return powers | power;
-  },
-
-  removePower(powers, power) {
-    return powers & ~power;
-  }
-};
 
 export default function Home() {
   return (
