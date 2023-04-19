ac0cc5140d006527d06e3ec328cbdf17c583b3a2
Christopher D Couture <c.couture@exsi.ca>
2023-04-19 07:10:28 -0400

    nextjs config updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-const isDev = process.env.NODE_ENV !== 'production';
-
 const nextConfig = {
   reactStrictMode: true,
-  basePath: isDev ? '' : '/AVLogicEditorJS',
-  assetPrefix: isDev ? '' : '/AVLogicEditorJS',
 }
 
 module.exports = nextConfig;
